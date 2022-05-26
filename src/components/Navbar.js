3f01bf6da3b9050f4a6f3ed84cdc1d23b155a4b8
Erick C <cherick04@hotmail.com>
2022-05-26 18:53:25 -0500

    Finished implementation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,11 @@
+import logo from './../images/react-icon-small.png'
+
 export default function Navbar() {
-  return <h1>Navbar goes here</h1>
+  return (
+    <nav>
+      <img src={logo} className="logo" alt="logo"></img>
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  )
 }
